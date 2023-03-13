1960694c61b4ec688db59910508921d1b8a290f9
wooil.jung <wooil.jung@lge.com>
2023-03-13 18:38:43 +0900

    init commit

--- other methods.py
+++ other methods.py
@@ -1,10 +1,19 @@
 import struct
+import chardet
+import codecs
 
 with open("savefile.sav", 'rb') as bdata:
-    jsonnum = struct.unpack('!HH', bdata.read(4))[0]
+    json_len = struct.unpack('<i', bdata.read(4))[0]
     data = bdata.read()
-    barray = bytearray(data)
+    # barray = bytearray(data)
+    json_content = data[4:json_len].decode('utf-8')
 
-decoded_data = data.decode('unicode_escape')
+    savedata_content = data[json_len+1:json_len+20]
+    # print(chardet.detect(savedata_content))
+    # print(savedata_content.decode('iso-8859-6'))
+    # print(struct.unpack('c', savedata_content))
 
-print(decoded_data)
+# print(savedata_content)
+# decoded_data = data.decode('unicode_escape')
+
+# print(decoded_data)
